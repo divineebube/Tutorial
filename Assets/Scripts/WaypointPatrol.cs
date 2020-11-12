60156183d245a0ca0d8aae09ae680b22eea93404
divineebube <divineebube@gmail.com>
2020-11-12 09:01:02 +0100

    Script

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+ 
